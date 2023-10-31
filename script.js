a95f8fd2614e28554674154b1c82ef66486fa2e3
nylRmon <nylraemon.leonardo@tup.edu.ph>
2023-10-31 14:18:42 +0800

    Update scrip.js
    Added script for header navbar.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.add('active');
+}
+
+document.querySelector('#close-navbar').onclick = () =>{
+  navbar.classList.remove('active');
+}
+
+
+
+
 const track = document.getElementById("image-track");
 
 const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
